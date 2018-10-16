264c72db6c6c361d116f5d9c66623b41b5e297d0
pinguwu <premixedcloud977@gmail.com>
2018-10-16 11:13:52 -0700

    skrrrr

--- webapp.py
+++ webapp.py
@@ -6,18 +6,35 @@ app = Flask(__name__)
 def render_main():
     return render_template('home.html')
 
-@app.route("EU")
+@app.route("/USDEUR")
 def render_money():
     return render_template('USDEUR.html')
 
-@app.route("LBG")
+@app.route("/pk")
 def render_weight():
     return render_template('pk.html')
 
-@app.route("FC")
+@app.route("/FC")
 def render_temp():
     return render_template('FC.html')
 
+@app.route("/response")
+def render_response():
+    fh = float(request.args['fh'])
+    lbs = float(request.args['lbs'])
+    usd = float(request.args['usd'])
+
+    if (fh != None or fh != ''):
+        ans = (fh - 32) * 5/9
+        return render_template('response.html', response = ans)
+
+    elif (lbs != None or lbs != ''):
+        ans = lbs / 2.205
+        return render_template('response.html', response = ans)
+
+    elif (usd != None or usd != ''):
+        ans = usd / 1.16
+        return render_template('response.html', response = ans)
 
 
 if __name__=="__main__":
